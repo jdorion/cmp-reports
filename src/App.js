8fbe8141ee835e322e76f71203c2daa2437daca2
J.T. Dorion <jdorion@gmail.com>
2018-06-29 14:08:58 -0400

    got prettier working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 
-
 class App extends React.Component {
-  render() {
-    return <div>Hello, world: {translations.text1}</div>;
-  }
+    render() {
+        return <div>Hello, world: {translations.text1}</div>;
+    }
 }
 
 export default hot(module)(App); // eslint-disable-line
